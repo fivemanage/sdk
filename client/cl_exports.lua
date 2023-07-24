d2d662415521efb90ca9bdcba20e3e5d173a658c
itschip <chip@projecterror.dev>
2023-07-24 18:47:47 +0200

    fix: remove test command

--- client/cl_exports.lua
+++ client/cl_exports.lua
@@ -1,10 +1,3 @@
-exports('takeServerImage', function(metadata)
-  TriggerServerEvent('fivemanage:server:takeImage', metadata)
+exports("takeServerImage", function(metadata)
+	TriggerServerEvent("fivemanage:server:takeImage", metadata)
 end)
-
-RegisterCommand('takeImage', function()
-  exports.fivemanage_lib:takeServerImage({
-    playerId = "1",
-    type = "player",
-  })
-end, false)
