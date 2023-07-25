fa289829074c48c4778b0896cc3bf7645f0bfbd7
Vytautas Serelis <vytutas2@gmail.com>
2023-07-25 13:09:59 +0300

    feat(img): exports to return request result data

--- server/sv_image.js
+++ server/sv_image.js
@@ -3,24 +3,35 @@ import fetch from 'node-fetch'
 
 const API_URL = "https://api.fivemanage.com/api/image"
 
-exports('takeServerImage', (src, metadata) => {
+exports('takeServerImage', async (src, metadata) => {
   if (!src) throw new Error("Player Id provided is not valid")
-  exports['screenshot-basic'].requestClientScreenshot(src, {
-    encoding: 'png',
-    quality: 0.85,
-  }, (_, data) => {
-    uploadImage(data, metadata)
+  
+  return await new Promise((resolve, reject) => { 
+    exports['screenshot-basic'].requestClientScreenshot(src, {
+      encoding: 'png',
+      quality: 0.85,
+    }, (_, data) => {
+      uploadImage(data, metadata).then(res => {
+        resolve(res)
+      })
+    })
   })
 })
 
-onNet('fivemanage:server:takeImage', (metadata) => {
+onNet('fivemanage:server:takeImage', (metadata, requestId) => {
   const src = global.source;
 
   exports['screenshot-basic'].requestClientScreenshot(src, {
     encoding: 'png',
     quality: 0.85,
   }, (_, data) => {
-    uploadImage(data, metadata)
+    uploadImage(data, metadata).then(result => {
+      emitNet('fivemanage:client:receiveImageCallback:'+requestId, src, result)
+      setTimeout(() => {
+        emitNet('fivemanage:client:receiveImageCallback:'+requestId, src, result)
+        
+      }, 1000);
+    })
   })
 })
 
@@ -42,14 +53,21 @@ function uploadImage(data, metadata) {
     form.append('metadata', JSON.stringify(metadata))
   }
 
-  fetch(API_URL, {
-    method: 'POST',
-    body: form,
-    headers: {
-      'Authorization': apiToken
-    }
-  })
-  .catch(err => {
-    console.log(err)
+  return new Promise((resolve, reject) => {  
+    fetch(API_URL, {
+      method: 'POST',
+      body: form,
+      headers: {
+        'Authorization': apiToken
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      resolve(json)
+    })
+    .catch(err => {
+      console.log(err)
+      reject(err)
+    })
   })
 }
