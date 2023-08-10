5819686f1835d221d5e3ac7fd797c86df87f5a08
itschip <chip@projecterror.dev>
2023-08-10 18:49:10 +0200

    fix(logs): fix requested changes

--- server/logs/lib.lua
+++ server/logs/lib.lua
@@ -1,10 +1,7 @@
 Logs = {}
-Logs.apiUrl= "https://api.fivemanagee.com/logs"
-Logs.apiKey = ""
+local apiUrl= "https://api.fivemanage.com/logs"
+local apiKey = GetConvar("FIVEMANAGE_API_KEY", "")
 
-function Logs:start()
-    Logs.apiKey = GetConvar("FIVEMANAGE_API_KEY", "")
-end
 
 -- TODO: Add context to logs
 function Logs.findContext()
@@ -13,20 +10,20 @@ function Logs.findContext()
     return info
 end
 
-function Logs:sendHttpRequest(data)
+local function sendHttpRequest(data)
 	PerformHttpRequest(
-        self.apiUrl,
+        apiUrl,
         function() end,
         "POST", 
         data = json.encode(data),
         {
-            Authorization = self.apiKey
+            Authorization = apiKey
         }
     )
 end
 
 function Logs:LogMessage(info, message, metadata)
-	self.findContext()
+	--self.findContext()
 	print("LogMessage: " .. info .. " " .. message)
 
     local log_data = {
@@ -36,51 +33,7 @@ function Logs:LogMessage(info, message, metadata)
         metadata = json.encode(metadata)
     }
 
-	self:sendHttpRequest(log_data)
+	sendHttpRequest(log_data)
 end
-
-function Logs:LogInfo(message, metadata)
-	self.findContext()
-	print("LogMessage: " .. info .. " " .. message)
-
-    local log_data = {
-        level = "info",
-        message = message,
-        -- TODO: We might not actually have to do this at all. One seralization might be enough
-        metadata = json.encode(metadata)
-    }
-
-	self:sendHttpRequest(log_data)
-end
-
-function Logs:LogWarn(message, metadata)
-	self.findContext()
-
-    local log_data = {
-        level = "warn",
-        message = message,
-        -- TODO: We might not actually have to do this at all. One seralization might be enough
-        metadata = json.encode(metadata)
-    }
-
-	self:sendHttpRequest(log_data)
-end
-
-function Logs:LogError(message, metadata)
-	self.findContext()
-
-    local log_data = {
-        level= "error",
-        message = message,
-        -- TODO: We might not actually have to do this at all. One seralization might be enough
-        metadata = json.encode(metadata)
-    }
-
-	self:sendHttpRequest(log_data)
-end
-
--- TODO: Add fatal level log
-
-Logs:start()
 
 print("LOGS ARE CURRENTLY IN BETA. IF YOU'D LIKE TO TEST, PLEASE JOIN OUR DISCORD SERVER")
