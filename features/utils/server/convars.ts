6091592428b4b08a939428fa4f1415bfd3719e3f
itschip <chip@projecterror.dev>
2025-05-10 18:58:25 +0200

    fix(convars): smol covnar change

--- features/utils/server/convars.ts
+++ features/utils/server/convars.ts
@@ -3,19 +3,9 @@ import {
 	ValiError,
 	flatten,
 	minLength,
-	object,
 	parse,
 	string,
 } from "valibot";
-
-const ConvarSchema = object({
-	FIVEMANAGE_LOGS_API_KEY: string([
-		minLength(1, "FIVEMANAGE_LOGS_API_KEY must be provided."),
-	]),
-	FIVEMANAGE_MEDIA_API_KEY: string([
-		minLength(1, "FIVEMANAGE_MEDIA_API_KEY must be provided."),
-	]),
-});
 
 export const FIVEMANAGE_LOGS_API_KEY = string([
 	minLength(1, "FIVEMANAGE_LOGS_API_KEY must be provided."),
@@ -25,31 +15,10 @@ export const FIVEMANAGE_MEDIA_API_KEY = string([
 	minLength(1, "FIVEMANAGE_MEDIA_API_KEY must be provided."),
 ]);
 
-function loadConvars(): Output<typeof ConvarSchema> {
-	try {
-		const convars = {
-			FIVEMANAGE_LOGS_API_KEY: GetConvar("FIVEMANAGE_LOGS_API_KEY", ""),
-			FIVEMANAGE_MEDIA_API_KEY: GetConvar("FIVEMANAGE_MEDIA_API_KEY", ""),
-		};
-
-		return parse(ConvarSchema, convars);
-	} catch (error) {
-		if (error instanceof ValiError) {
-			console.error(
-				"❌ Invalid configuration variables:\n",
-				flatten<typeof ConvarSchema>(error).nested,
-			);
-
-			throw new Error("Invalid configuration variables");
-		}
-
-		throw new Error("Error loading configuration variables");
-	}
-}
-
 export function loadLogsConvar(): Output<typeof FIVEMANAGE_LOGS_API_KEY> {
 	try {
-		return parse(FIVEMANAGE_LOGS_API_KEY, GetConvar("FIVEMANAGE_LOGS_API_KEY", ""));
+		const apiKey = GetConvar("FIVEMANAGE_LOGS_API_KEY", "");
+		return parse(FIVEMANAGE_LOGS_API_KEY, apiKey);
 	}
 	catch (error) {
 		if (error instanceof ValiError) {
@@ -67,7 +36,8 @@ export function loadLogsConvar(): Output<typeof FIVEMANAGE_LOGS_API_KEY> {
 
 export function loadMediaConvar(): Output<typeof FIVEMANAGE_MEDIA_API_KEY> {
 	try {
-		return parse(FIVEMANAGE_MEDIA_API_KEY, GetConvar("FIVEMANAGE_MEDIA_API_KEY", ""));
+		const apiKey = GetConvar("FIVEMANAGE_MEDIA_API_KEY", "");
+		return parse(FIVEMANAGE_MEDIA_API_KEY, apiKey);
 	}
 	catch (error) {
 		if (error instanceof ValiError) {
